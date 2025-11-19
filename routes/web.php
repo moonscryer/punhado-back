38ab3406b54715c65289c4dd04b61acb6d17b341
Felipe Tavares <felpbanks@gmail.com>
2025-11-19 01:35:41 +0100

    models & test dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,5 @@ Route::view('/about', 'about')->name('about');
 Route::fallback(function(){
     return "Page not found. Return <a href='/'>home</a>.";
 })->name('fallback');
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
