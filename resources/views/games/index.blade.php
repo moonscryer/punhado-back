5c25121e17f4f009fef4a6dc7587918f1385bf19
Felipe Tavares <felpbanks@gmail.com>
2025-11-25 06:09:28 +0100

    modal change

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -53,7 +53,7 @@
 <div id="deleteModal" class="fixed inset-0 bg-gray-800 bg-opacity-50 hidden items-center justify-center z-50">
     <div class="bg-white rounded-lg shadow-lg w-96 p-6">
         <h2 class="text-xl font-bold mb-4">Confirm Delete</h2>
-        <p class="mb-4">Are you sure you want to delete this game? This action cannot be undone.</p>
+        <p>Are you sure you want to delete this game?</p><p class="mb-4 text-red-700 font-bold">This action cannot be undone and will delete all characters associated with this game.</p>
         <div class="flex justify-end space-x-2">
             <button onclick="closeDeleteModal()" class="px-4 py-2 rounded bg-gray-300 hover:bg-gray-400">Cancel</button>
             <form id="deleteForm" method="POST" class="inline">
