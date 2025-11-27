d437f087a297d06071b7d4d97f9c899a299c2b38
Felipe Tavares <felpbanks@gmail.com>
2025-11-27 07:40:12 +0100

    api installed

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable; // ← Add HasApiTokens here
 
     protected $fillable = [
         'username',
